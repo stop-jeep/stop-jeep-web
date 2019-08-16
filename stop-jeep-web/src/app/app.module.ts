7950f3cb8286e02a593aaa960c4aac646c1602b1
raksit31667 <raksit.m@ku.th>
2019-08-16 22:18:50 +0700

    added feature get total balance by date-time

--- stop-jeep-web/src/app/app.module.ts
+++ stop-jeep-web/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DlDateTimeDateModule, DlDateTimePickerModule } from 'angular-bootstrap-datetimepicker';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
+    DlDateTimeDateModule,
+    DlDateTimePickerModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- stop-jeep-web/src/app/home/home/home.component.ts
+++ stop-jeep-web/src/app/home/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from 'src/app/data-service.service';
 import { Observable } from 'rxjs';
 import { BankAccount } from 'src/app/bankAcccount';
+import { DlDateTimePickerChange } from 'angular-bootstrap-datetimepicker';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -11,20 +13,30 @@ import { BankAccount } from 'src/app/bankAcccount';
 export class HomeComponent implements OnInit {
   public balance: number;
   private userAccount: BankAccount;
+  private name: String;
+
+  maxView = 'year';
+  minuteStep = 5;
+  minView = 'minute';
+  selectedDate: Date;
+  selectedDateUTC: string;
+  showCalendar = true;
+  startView = 'day';
+  views = ['minute', 'hour', 'day', 'month', 'year'];
   balance$: Observable<any>;
+  balanceByDateTime$: Observable<any>;
 
   constructor(
     private dataService: DataServiceService
   ) { }
 
   ngOnInit() {
-    console.log('hello');
+    this.name = 'Mr. Jeep';
     this.balance$ = this.dataService.getBalance('b');
-    // this.getBalance(this.userAccount.userId);
   }
 
-  getBalance(userId: string): void {
-    this.balance$ = this.dataService.getBalance(userId);
+  onCustomDateChange(event: DlDateTimePickerChange<Date>) {
+    this.selectedDateUTC = moment.utc(this.selectedDate).format('YYYY-MM-DDTHH:mm:ss');
+    this.balanceByDateTime$ = this.dataService.getBalanceByDateTime('b', this.selectedDateUTC);
   }
-
 }
